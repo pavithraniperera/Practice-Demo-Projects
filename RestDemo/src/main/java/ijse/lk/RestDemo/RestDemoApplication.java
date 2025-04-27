f2eb98be4d725885c2d0612f8a757e810f833143
pavithrani <pererapavithrani02@gmail.com>
2025-04-27 11:57:30 +0530

    Spring boot pojo practice

--- RestDemo/src/main/java/ijse/lk/RestDemo/RestDemoApplication.java
+++ RestDemo/src/main/java/ijse/lk/RestDemo/RestDemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestDemoApplication.class, args);
 	}
