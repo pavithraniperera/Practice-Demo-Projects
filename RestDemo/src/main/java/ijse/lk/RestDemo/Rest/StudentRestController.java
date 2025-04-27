4c9710bb69a83fe6cc36a92dc024268609d5f056
pavithrani <pererapavithrani02@gmail.com>
2025-04-27 19:04:51 +0530

    Spring boot pathvariable practice

--- RestDemo/src/main/java/ijse/lk/RestDemo/Rest/StudentRestController.java
+++ RestDemo/src/main/java/ijse/lk/RestDemo/Rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package ijse.lk.RestDemo.Rest;
 
 import ijse.lk.RestDemo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,17 +13,33 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> theStudents;
 
-    @GetMapping("/student")
-    public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<>();
+    //define @PostConstruct to load the student data only once
+    @PostConstruct
+    public void loadData(){
+         theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
 
+
+    }
+
+    @GetMapping("/student")
+    public List<Student> getStudents(){
+
+
         return theStudents;
     }
+    @GetMapping("/student/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        // simply access by index of the list
+        return theStudents.get(studentId);
+
+    }
+
 
 
 }
