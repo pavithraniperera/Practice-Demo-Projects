f7264f75934f8a6bd3aab86e5c62a365fbad7119
pavithrani <pererapavithrani02@gmail.com>
2025-04-28 10:28:50 +0530

    practice Rest CRUD operations

--- RestCrud/src/main/java/com/example/restcrud/rest/EmployeeRestController.java
+++ RestCrud/src/main/java/com/example/restcrud/rest/EmployeeRestController.java
@@ -53,4 +53,23 @@ public class EmployeeRestController {
 
         return dbEmployee;
     }
+
+    //delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
+
 }
+
