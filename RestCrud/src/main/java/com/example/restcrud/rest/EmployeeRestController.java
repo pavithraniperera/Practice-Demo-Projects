8fb36670074a1663c6df065b2d81ae7b5b47af27
pavithrani <pererapavithrani02@gmail.com>
2025-04-28 09:50:05 +0530

    practice Rest CRUD operations

--- RestCrud/src/main/java/com/example/restcrud/rest/EmployeeRestController.java
+++ RestCrud/src/main/java/com/example/restcrud/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.example.restcrud.Dao.EmployeeDao;
 import com.example.restcrud.entity.Employee;
 import com.example.restcrud.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,17 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
     }
 }
