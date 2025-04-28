2b290e9701e06d3c69c1b4c1be420c73b276e70e
pavithrani <pererapavithrani02@gmail.com>
2025-04-28 11:53:29 +0530

    practice  Data REST Configs

--- RestCrud/src/main/java/com/example/restcrud/DaoWithJPA/EmployeeRepository.java
+++ RestCrud/src/main/java/com/example/restcrud/DaoWithJPA/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.example.restcrud.DaoWithJPA;
 
 import com.example.restcrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//localhost:8080/magic-api/members
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
     //no need to write any implementation by manually
 }
