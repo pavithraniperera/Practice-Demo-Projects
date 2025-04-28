f1af120d341466f45e36c6b4ae738d3424ffbacc
pavithrani <pererapavithrani02@gmail.com>
2025-04-28 09:39:29 +0530

    practice Rest CRUD operations

--- RestCrud/src/main/java/com/example/restcrud/service/EmployeeService.java
+++ RestCrud/src/main/java/com/example/restcrud/service/EmployeeService.java
@@ -6,4 +6,9 @@ import java.util.List;
 
 public interface EmployeeService {
     List<Employee> findAll();
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }
