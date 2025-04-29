a18eefacbf4a76331889739efb7c48c8281a6c36
pavithrani <pererapavithrani02@gmail.com>
2025-04-29 13:02:03 +0530

    Add Employee UI with Thymeleaf

--- RestCrud/src/main/java/com/example/restcrud/Controller/EmployeeController.java
+++ RestCrud/src/main/java/com/example/restcrud/Controller/EmployeeController.java
@@ -5,7 +5,9 @@ import com.example.restcrud.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.List;
 
@@ -31,5 +33,25 @@ public class EmployeeController {
         theModel.addAttribute("employees", theEmployees);
 
         return "list-employees";
+    }
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+
+        // create model attribute to bind form data
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+        // save the employee
+        employeeService.save(theEmployee);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
     }
 }
