21222d65c85cb0fd8d17c722ef21e54190d067df
pavithrani <pererapavithrani02@gmail.com>
2025-04-29 10:13:25 +0530

    Data binding with dropDown list

--- RestDemo/src/main/java/ijse/lk/RestDemo/Controller/StudentController.java
+++ RestDemo/src/main/java/ijse/lk/RestDemo/Controller/StudentController.java
@@ -1,14 +1,19 @@
 package ijse.lk.RestDemo.Controller;
 
 import ijse.lk.RestDemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
     //method to show our form
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -18,6 +23,7 @@ public class StudentController {
 
         // add student object to the model
         theModel.addAttribute("student", theStudent);
+        theModel.addAttribute("countries",countries);
 
         return "student-form";
     }
--- RestDemo/src/main/java/ijse/lk/RestDemo/model/Student.java
+++ RestDemo/src/main/java/ijse/lk/RestDemo/model/Student.java
@@ -3,10 +3,12 @@ package ijse.lk.RestDemo.model;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 
-    public Student(String firstName, String lastName) {
+    public Student(String firstName, String lastName,String country) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.country=country;
     }
 
     public Student(){
@@ -27,5 +29,13 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
