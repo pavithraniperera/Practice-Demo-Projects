978308afe2d5755c884d365b2c957e8790124f4c
pavithrani <pererapavithrani02@gmail.com>
2025-04-30 12:39:29 +0530

    develop main app

--- CRUDDemo/src/main/java/lk/ijse/cruddemo/CrudDemoApplication.java
+++ CRUDDemo/src/main/java/lk/ijse/cruddemo/CrudDemoApplication.java
@@ -1,5 +1,8 @@
 package lk.ijse.cruddemo;
 
+import lk.ijse.cruddemo.Entity.Instructor;
+import lk.ijse.cruddemo.Entity.InstructorDetail;
+import lk.ijse.cruddemo.dao.AppDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,11 +15,49 @@ public class CrudDemoApplication {
 		SpringApplication.run(CrudDemoApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner commandLineRunner() {
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
 		return runner -> {
-			System.out.println("Hello");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+
+		/*
+		// create the instructor
+		Instructor tempInstructor =
+				new Instructor("Chad", "Darby", "darby@luv2code.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail(
+						"http://www.luv2code.com/youtube",
+						"Luv 2 code!!!");
+		*/
+
+		// create the instructor
+		Instructor tempInstructor =
+				new Instructor("Madhu", "Patel", "madhu@luv2code.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail(
+						"http://www.luv2code.com/youtube",
+						"Guitar");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor
+		//
+		// NOTE: this will ALSO save the details object
+		// because of CascadeType.ALL
+		//
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
 	}
 
 }
