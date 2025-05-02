11ead4445cf389b69a27c4d360150f880bbdee03
pavithrani <pererapavithrani02@gmail.com>
2025-05-02 10:03:38 +0530

    initialize AOPdemo project

---
+++ ApoDemo/src/main/java/com/code/apodemo/ApoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.code.apodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApoDemoApplication.class, args);
+	}
+
+}
